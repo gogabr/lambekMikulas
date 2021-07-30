c6cc4413aaac7bc6d1e84bd661e9d5bdd5b04931
Georgy Bronnikov <georgy.bronnikov@jetbrains.com>
2021-07-30 15:30:10 +0300

    TMP2

--- Lambek.v
+++ Lambek.v
@@ -54,7 +54,11 @@ Section LambekCalculus.
                 (p2: GenProof Γ ((Y::y) ⇒ B))
        : GenProof Γ ((X :: x ++ Y :: y) ⇒ A ° B).
 
-  Notation "Γ ⊢ s" := (GenProof Γ s) (at level 60, no associativity).
+  Definition ProofWithNonEmptyPremises (Γ: list sequent) (s: sequent): Prop :=
+    (forall (s': sequent), In s' Γ -> nonEmptySequent s') /\
+    GenProof Γ s.
+
+  Notation "Γ ⊢ s" := (ProofWithNonEmptyPremises Γ s) (at level 60, no associativity).
 
    (* Lemma FormulaProof (x: str) (X: Formula) (C: Formula):  *)
    (*   Proof (x ++ [X], C) <-> Proof ([fold_right mul X x], C). *)
@@ -128,7 +132,7 @@ Section LambekCalculus.
 
      Definition seqValuation (s: sequent): subW :=
        match s with
-       | (X :: x, A) => fun p => (formValuation A p)  \/ not (strValuation X x p)
+       | (X :: x, A) => fun p => (strValuation X x p) ->  (formValuation A p)
        | ([], A) => fun _ => False
        end.
 
@@ -177,11 +181,46 @@ Section LambekCalculus.
          exists z. split. 1:assumption.
          unfold C. auto.
      Qed.
+
+     Fixpoint pullMultRight (X : Formula) (x : str) :=
+       match x with
+       | [] => X
+       | (X'::x') => X ° (pullMultRight X' x')
+       end.
+
+     Lemma pullMultRightExtraction (A  X: Formula) (x : str):
+       forall p,
+         formValuation (pullMultRight (A ° X) x) p <-> formValuation (A ° pullMultRight  X x) p.
+     Proof.
+       generalize dependent A. generalize dependent X.
+       induction x as [| X' x].
+       - intros. reflexivity.
+       - intros.
+       set (TG1 := (pullMultRight (A ° X) (X' :: x))).
+       set (TG2 := (A ° pullMultRight X (X' :: x))).
+       simpl in TG1. simpl in TG2.
+       apply strAssoc.
+     Qed.
+
+     Lemma strValuationToFormValuationRight: forall A x p,
+         strValuation A x p <-> formValuation (pullMultRight A x) p.
+     Proof.
+       intros.
+       generalize dependent A.
+       induction x as [| X x].
+       - simpl. reflexivity.
+       - intros.
+         set (TG := strValuation  A (X :: x) p).
+         simpl in TG.
+         subst TG.
+         rewrite (IHx (A ° X)).
+         apply pullMultRightExtraction.
+     Qed.
    End Semantics.
 
    Definition semConsequence (Γ: list sequent) (s: sequent) :=
      (forall s', In s' Γ -> nonEmptySequent s') /\
-     forall (U: Set) (W: relation U) (v: types -> subW U W),
+     forall (U: Set) (W: relation U) (t: transitive U W) (v: types -> subW U W),
        (forall s', (In s' Γ) -> seqTrue U W v s') -> seqTrue U W v s.
 
    Notation "Γ ⊨ s" := (semConsequence Γ s) (at level 60, no associativity).
@@ -191,6 +230,14 @@ Section LambekCalculus.
    Definition AllSingleton (_:Wpoint Singleton TotalOnSingleton) := True.
    Definition ValOnSingleton := fun (_:types) => AllSingleton.
    Definition WpointOnSingleton := exist (fun (_: Singleton * Singleton) => True) (Sing, Sing) I.
+
+   Lemma TransitiveOnSingleton : transitive Singleton TotalOnSingleton.
+   Proof.
+     unfold transitive.
+     intros x y z T1 T2.
+     unfold TotalOnSingleton.
+     exact I.
+   Qed.
 
    Lemma AllEqualOnSingleton: forall (x y: Wpoint Singleton TotalOnSingleton), x = y.
    Proof.
@@ -227,8 +274,8 @@ Section LambekCalculus.
        intros. unfold seqTrue. intros.  unfold seqValuation. destruct s.
        assert (nonEmptySequent (s ⇒ f)) as NE by apply NEmp, H. unfold nonEmptySequent in NE.
        destruct s. 1:{ apply NE; reflexivity. }
-       left.  apply AllTrueOnSingleton. }
-     apply (STrue Singleton TotalOnSingleton ValOnSingleton) in H.
+       intro.  apply AllTrueOnSingleton. }
+     apply (STrue Singleton TotalOnSingleton TransitiveOnSingleton ValOnSingleton) in H.
      unfold seqTrue in H.
      assert (seqValuation Singleton TotalOnSingleton ValOnSingleton ([] ⇒ A) WpointOnSingleton) as HH by apply H.
      apply HH.
@@ -250,8 +297,8 @@ Section LambekCalculus.
      - intro. simpl. rewrite (IHx  (X ° a)). reflexivity.
    Qed.
 
-   Lemma strValuationToFormValuation: forall U W v X x p,
-       strValuation U W v X x p = formValuation U W v (pullMultLeft X x) p.
+   Lemma strValuationToFormValuationLeft: forall U W v X x p,
+       strValuation U W v X x p <-> formValuation U W v (pullMultLeft X x) p.
    Proof.
      intros.
      generalize dependent X.
@@ -266,55 +313,150 @@ Section LambekCalculus.
      unfold semConsequence. intros.
      unfold seqTrue. split.
      - unfold semConsequence in H. destruct H. assumption.
-     - intros U W v allΓ p.
+     - intros U W t v allΓ p.
        assert (x <> []) as XnonEmpty by (apply (conseqNonEmpty Γ x A); assumption).
        unfold seqValuation.
        destruct x as [| X x]. 1:{ exfalso. apply XnonEmpty. reflexivity. }
-       simpl. apply or_comm. apply imply_to_or.  intros StrVal. simpl.
+       simpl.  intros StrVal. simpl.
        rewrite strValuationRight in StrVal.
        simpl in StrVal.
        destruct StrVal as [p1 [FVpml [p2 [XX1 XX2]]]].
        unfold semConsequence in H.
-       destruct H as [_ H]. set (HH := H U W v allΓ p1) . unfold seqTrue in HH.
-       unfold seqValuation in HH.  rewrite strValuationToFormValuation in HH.
-       apply or_comm in HH. set (HHH:= or_to_imply _ _ HH).
+       destruct H as [_ H]. set (HH := H U W t v allΓ p1) . unfold seqTrue in HH.
+       unfold seqValuation in HH.  rewrite strValuationToFormValuationLeft in HH.
        apply (XX1 p1). 2:assumption.
-       apply HHH. assumption.
+       apply HH. assumption.
    Qed.
 
-   Fixpoint pullMultRight (X : Formula) (x : str) :=
-     match x with
-     | [] => X
-     | (X'::x') => X ° (pullMultRight X' x')
-     end.
-
-   Lemma strValuationLeft: forall U W v A x p,
-       strValuation U W v A x p = formValuation U W v (pullMultRight A x) p.
-   Proof.
-     intros.
-     generalize dependent A.
-     induction x as [| X x].
-     - simpl. reflexivity.
-     - simpl. intros. rewrite (IHx (A ° X)).
-!!!! From here !!!!!
-
-       Lemma rightDivSound: forall Γ x A B, Γ ⊨ x ⇒ A -> Γ ⊨ (((A // B) :: x) ⇒ B).
+   Lemma rightDivSound: forall Γ x A B, Γ ⊨ x ⇒ A -> Γ ⊨ (((B // A) :: x) ⇒ B).
    Proof.
      intros.
      unfold semConsequence. intros.
      unfold seqTrue. split.
      - unfold semConsequence in H. destruct H. assumption.
-     - intros U W v allΓ p.
+     - intros U W t v allΓ p.
        assert (x <> []) as XnonEmpty by (apply (conseqNonEmpty Γ x A); assumption).
-       simpl.
-       apply or_comm. apply imply_to_or. intros StrVal.
+       simpl. intros StrVal.
        destruct x as [| X x]. 1: congruence.
        simpl in StrVal.
-       unfold strValuation in StrVal.
+       apply strValuationToFormValuationRight in StrVal.
+       apply pullMultRightExtraction in StrVal. 2,3: apply t.
+       simpl in StrVal.
+       destruct StrVal as [p1 [R1 [p2 [FV2 C12p]]]].
+       assert (formValuation U W v A p2) as AatP2. {
+         unfold semConsequence in H.
+          destruct H as [AllNonEmpty HH].
+          set (HHH := (HH U W t v allΓ)).
+          unfold seqTrue in HHH. unfold seqValuation in HHH.
+          apply (HHH p2).
+          apply strValuationToFormValuationRight. 1:apply t.
+          apply FV2. }
+       apply R1 with (y:=p2). 1:apply AatP2.
+       apply C12p.
+   Qed.
 
-     Lemma Soundness: forall Γ s, Γ ⊢ s -> Γ ⊨ s.
+   Lemma Soundness: forall Γ s, Γ ⊢ s -> Γ ⊨ s.
    Proof.
+     intros Γ s [NE HH].
+     induction HH as [ s inΓ NE2 | A | y X x A B z CC | | | | | ].
+     - unfold semConsequence. split. 1:apply NE.
+       intros U W t v allΓ.
+       apply allΓ, inΓ.
+     - unfold semConsequence. split. 1: apply NE.
+       intros U W t v allΓ p.
+       unfold seqValuation. unfold strValuation.
+       intro. assumption.
+     - unfold semConsequence. split. 1: apply NE.
+       intros U W t v allΓ p.
+       unfold seqValuation.
+       induction y.
+       + simpl. intros strVal.
+           generalize dependent B.
+           induction z as [| Z z].
+         * intros B _ IHHH2 strVal.
+           simpl in IHHH2.
+           unfold semConsequence in IHHH2.
+           destruct IHHH2 as [_ IHHH2].
+           set (BtoCC := IHHH2 U W t v allΓ p).
+           unfold seqValuation in BtoCC. apply BtoCC.
+           unfold strValuation.
+           destruct IHHH1 as [_ IHHH1].
+           rewrite strValuationRight in strVal.
+           unfold strValuation in strVal.
+           simpl in strVal.
+           destruct strVal as [p1 [FVA [p2 [RR C12p]]]].
+           set (IHH1S := IHHH1 U W t v allΓ p1).
+           unfold seqValuation in IHH1S.
+           rewrite strValuationToFormValuationLeft in IHH1S.
+           set (FVAA := IHH1S FVA).
+           set (TG := RR p1 FVAA p C12p).
+           assumption.
+         * intros B HH2 IHHH2 StrVal.
+           simpl. simpl in IHHH2.
+           apply (IHz (B ° Z)).
+           **  apply (mulArrow Γ [] B Z z CC). assumption.
+           **  unfold semConsequence. split. 1: apply NE.
    Admitted.
+
+   Lemma MulInSeqTrue U W v x A B z CC:
+     seqTrue U W v ((x ++ (A ° B :: z)) ⇒ CC) <-> seqTrue U W v ((x ++ A :: B :: z) ⇒ CC).
+   Proof.
+     generalize dependent CC.
+     induction x as [| X x].
+     - intros CC. simpl.
+       unfold seqTrue.
+       split.
+       + intros H p.
+           unfold seqValuation.
+           intros SV.
+           unfold seqValuation in H.
+           simpl in SV.
+           apply H.
+           assumption.
+       + intros H p.
+           unfold seqValuation.
+           intros SV.
+           unfold seqValuation in H.
+           set (HH := H p).
+           simpl in HH.
+           apply (HH SV).
+     - intros CC. simpl.
+       set (IHXC := IHx (X \\ CC)).
+   Admitted.
+
+   Lemma arrowLeftSound U W (t: transitive U W) v x A B:
+     x <> [] ->
+     seqTrue U W v ((A :: x) ⇒ B) <-> seqTrue U W v (x ⇒ A \\ B).
+   Proof.
+     intros NE.
+     unfold seqTrue.
+     split.
+     - intros H p.
+       unfold seqValuation.
+       destruct x as [| X x]. 1: congruence.
+       unfold seqValuation in H.
+       intros SV.
+       simpl.
+       intros p1 FVA p2 C1p2.
+       set (HH := H p2).
+       apply HH.
+       apply strValuationToFormValuationRight. 1: apply t.
+       simpl.
+       exists p1. split. 1: assumption.
+       exists p. split. 2: assumption.
+       apply strValuationToFormValuationRight in SV. 2: apply t.
+       assumption.
+     - intros H p.
+       unfold seqValuation.
+       destruct x as [|X x]. 1: congruence.
+       intros SV.
+       simpl in H.
+       apply strValuationToFormValuationRight in SV. 2: apply t.
+       simpl in SV.
+       destruct SV as [p1 [FVA [p2 [FVx C12p]]]].
+       apply strValuationToFormValuationRight in FVx. 2: apply t.
+       apply (H p2 FVx p1 FVA p C12p).
+   Qed.
 
    Lemma Completeness: forall Γ s, Γ ⊨ s -> Γ ⊢ s.
    Admitted.
